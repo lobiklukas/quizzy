90662c45f8b4f0500c49b553b1ca98a5eb754ef8
Lukáš Lobík <lobiklukas@gmail.com>
2023-01-17 17:52:16 +0100

    Change manifest bg to transparent

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -10,12 +10,15 @@ class MyDocument extends Document {
           <meta name="description" content="Quizzy" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <link rel="manifest" href="/manifest.json" />
+          <link rel="apple-touch-icon" href="/icon.png" />
           <meta name="application-name" content="Quizzy" />
           <meta name="apple-mobile-web-app-title" content="Quizzy" />
           <meta name="apple-mobile-web-app-capable" content="yes" />
           <meta name="mobile-web-app-capable" content="yes" />
-          <meta name="apple-mobile-web-app-status-bar-style" content="black" />
-          <link rel="apple-touch-icon" href="/icon.png" />
+          <meta
+            name="apple-mobile-web-app-status-bar-style"
+            content="black-translucent"
+          />
         </Head>
         <body>
           <Main />
