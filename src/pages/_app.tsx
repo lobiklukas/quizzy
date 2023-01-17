4580bc7ce9590f9191272161ebf46feebbbfc52f
Lukáš Lobík <lobiklukas@gmail.com>
2023-01-17 18:17:50 +0100

    Move meta tags to _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,6 +7,7 @@ import { trpc } from "../utils/trpc";
 import "../styles/globals.css";
 import { useEffect } from "react";
 import { themeChange } from "theme-change";
+import Head from "next/head";
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
@@ -16,11 +17,24 @@ const MyApp: AppType<{ session: Session | null }> = ({
     themeChange(false);
   }, []);
   return (
-    <SessionProvider session={session}>
-      <div>
-        <Component {...pageProps} />
-      </div>
-    </SessionProvider>
+    <>
+      <Head>
+        <meta name="description" content="Quizzy" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icon.png" />
+        <meta name="application-name" content="Quizzy" />
+        <meta name="apple-mobile-web-app-title" content="Quizzy" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+      </Head>
+      <SessionProvider session={session}>
+        <div>
+          <Component {...pageProps} />
+        </div>
+      </SessionProvider>
+    </>
   );
 };
 
