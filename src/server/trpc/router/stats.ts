5f13aa3f559692090d582b4db05432b43f172dec
Lukáš Lobík <lobiklukas@gmail.com>
2023-07-14 16:33:49 +0200

    Use prisma transaction for stats

--- src/server/trpc/router/stats.ts
+++ src/server/trpc/router/stats.ts
@@ -3,9 +3,11 @@ import { publicProcedure, router } from "../trpc";
 export const statsRouter = router({
   getAllStats: publicProcedure
     .query(async ({ ctx }) => {
-      const learnedCards = await ctx.prisma?.question.count();
-      const registeredUsers = await ctx.prisma?.account.count();
-      const numOfQuizes = await ctx.prisma?.quiz.count();
+      const [learnedCards, registeredUsers, numOfQuizes] = await ctx.prisma.$transaction([
+        ctx.prisma?.question.count(),
+        ctx.prisma?.account.count(),
+        ctx.prisma?.quiz.count()
+      ])
       return {
         registeredUsers,
         learnedCards,
